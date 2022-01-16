bcfcfd930b79bccfcd3e0896c82eaecee221e8f5
baggio-snir <71394182+baggio-snir@users.noreply.github.com>
2022-01-16 14:38:55 +0100

    Question 3
    Ajout des deux tableaux avec les summaries
    Respect de la syntaxe table/thead/tbody

--- index.php
+++ index.php
@@ -22,7 +22,35 @@ if(!empty($_REQUEST['addSale'])) {
         <p>
             Ce gestionnaire est un test réalisé par <strong>M. Chassel</strong>.
         </p>
-        
+        <details>
+            <summary>Stocks</summary>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Couleur</th>
+                        <th>Marque</th>
+                        <th>Prix</th>
+                        <th>Date d'entrée</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </details>
+        <details>
+            <summary>Ventes</summary>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date de vente</th>
+                        <th>Couleur</th>
+                        <th>Marque</th>
+                        <th>Bénéfice</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </details>
         <footer>
             Copyright
             <span title="M. Chassel">Baggio SNIR</span>
